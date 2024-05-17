94a094b95b07315f0c0ff123fff1120be0d2f915
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-17 18:22:32 +0300

    added the style for the SideBar component

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,8 +2,16 @@ import "./SideBar.css"
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
-  )
+    <div className="side-bar-container">
+      <ul className="side-bar-tags">
+        <li><i className="fa-solid fa-house fa-xl"></i>Home</li>
+        <li><i className="fa-solid fa-calendar-days fa-xl"></i>Calendar</li>
+        <li><i className="fa-solid fa-square-check fa-xl"></i>Add Event</li>
+        <li><i className="fa-solid fa-address-book fa-xl"></i>Contacts</li>
+        <li><i className="fa-solid fa-gear fa-xl"></i>Settings</li>
+      </ul>
+    </div>
+  );
 }
 
 export default SideBar
