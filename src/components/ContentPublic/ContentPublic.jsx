d5b1c2f0d442461fdde3048892815ee94dc3a7d8
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-17 14:42:20 +0300

    added some text ot style the page

--- src/components/ContentPublic/ContentPublic.jsx
+++ src/components/ContentPublic/ContentPublic.jsx
@@ -15,8 +15,17 @@ const ContentPublic = () => {
         <img
           className="content-public-img"
           src="https://picsum.photos/900/500"
-            alt="demo"
+          alt="demo"
         />
+      </div>
+      <div className="partners">
+        <h3>Simplified scheduling for more than 20 million users worldwide</h3>
+        <div className="partners-logos">
+          <img src="https://picsum.photos/200/50" alt="partner" />
+          <img src="https://picsum.photos/200/50" alt="partner" />
+          <img src="https://picsum.photos/200/50" alt="partner" />
+          <img src="https://picsum.photos/200/50" alt="partner" />
+        </div>
       </div>
     </div>
   );
