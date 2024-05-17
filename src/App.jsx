b06950db347a712fb65112070b056da2a934c5cd
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-17 18:23:27 +0300

    removed the Header component from PublicView and added it to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PublicView from './views/PublicView/PublicView';
+import Home from './views/Home/Home';
+import Header from './components/Header/Header';
+import SideBar from './components/SideBar/SideBar';
 
 function App() {
 
+
   return (
     <BrowserRouter>
+      <Header />
+      {window.location.pathname !== "/" && <SideBar />}
       <Routes>
-        <Route path="/" element={<PublicView />} />
+        <Route path="/" element=<PublicView /> />
+        <Route path="/home" element=<Home /> />
       </Routes>
     </BrowserRouter>
   )
--- src/views/PublicView/PublicView.jsx
+++ src/views/PublicView/PublicView.jsx
@@ -1,11 +1,9 @@
 import "./PublicView.css"
-import Header from "../../components/Header/Header"
 import ContentPublic from "../../components/ContentPublic/ContentPublic"
 
 const PublicView = () => {
   return (
     <div>
-        <Header />
         <ContentPublic />
     </div>
   )
