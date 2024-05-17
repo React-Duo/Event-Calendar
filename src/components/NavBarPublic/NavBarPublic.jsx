cf7a606b1c581113a6a21afd2fd049fa7b4f7f9b
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-17 12:42:54 +0300

    the navBar for not logged users

--- src/components/NavBarPublic/NavBarPublic.jsx
+++ src/components/NavBarPublic/NavBarPublic.jsx
@@ -1,8 +1,20 @@
 import "./NavBarPublic.css"
+import { assets } from "../../assets/assets"
 
 const NavBarPublic = () => {
   return (
-    <div>NavBarPublic</div>
+    <>
+    <div className="nav-bar-public">
+        <div className="nav-bar-public-logo">
+            <img src={assets.logo} alt="logo" />
+        </div>
+        <div className="nav-bar-public-menu">
+            <a id="loginBtn" href="/login">Log in</a>
+            <a id="registerBtn" href="/signup">Get started</a>
+        </div>
+    </div>
+    <hr />
+    </>
   )
 }
 
