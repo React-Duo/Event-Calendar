f880451b57a56c52c53c3cb68e09deb32cd7a3b2
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-17 12:42:29 +0300

    the public view for not logged users

--- src/views/PublicView/PublicView.jsx
+++ src/views/PublicView/PublicView.jsx
@@ -1,10 +1,12 @@
 import "./PublicView.css"
 import NavBarPublic from "../../components/NavBarPublic/NavBarPublic"
+import ContentPublic from "../../components/ContentPublic/ContentPublic"
 
 const PublicView = () => {
   return (
     <div>
         <NavBarPublic />
+        <ContentPublic />
     </div>
   )
 }
