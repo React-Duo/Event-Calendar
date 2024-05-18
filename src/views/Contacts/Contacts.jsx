2fa33bf2e9408cf19cdd753534ac9acf94ada489
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-18 19:45:44 +0300

    removed the code for AddList into a separated component

--- src/views/Contacts/Contacts.jsx
+++ src/views/Contacts/Contacts.jsx
@@ -1,5 +1,6 @@
 import "./Contacts.css";
 import { useState } from "react";
+import AddList from "../../components/AddList/AddList";
 
 const Contacts = () => {
   const [showNewList, setShowNewList] = useState(false);
@@ -16,45 +17,14 @@ const Contacts = () => {
       </div>
       <div className="all-lists">
         <div className="single-list">
-          <p>Petkoe213</p>
-        </div>
-      </div>
-      <div className="container-list-form">
-        {showNewList ? (
-          <div className="add-list">
-            <div className="add-list__header">
-              <span className="add-list__title">New list</span>
-              <button onClick={handleShowNewList} className="button--icon">x</button>
-            </div>
-            <div className="add-list__body">
-              <div className="input">
-                <label className="input__label">List name</label>
-                <input className="input__field" type="text" placeholder="Enter list name" />
-              </div>
-              <div className="input">
-                <label className="input__label">Members</label>
-                <input className="input__field" type="search" placeholder="Add members by email" />
-              </div>
-            </div>
-            <div className="container-find-contact">
-              <div className="single-contact">
-                <div>
-                  <img src="https://picsum.photos/40/40"></img>
-                </div>
-                <div>
-                  <p>petko1234@gmail.com</p>
-                </div>
-                <button>Add</button>
-              </div>
-            </div>
-            <div className="add-list__footer">
-              <button className="button--primary">Create list</button>
-            </div>
+          <div><p>My team</p></div>
+          <div className="notifier">
+          <i className="fa-solid fa-user-group"></i>
+          <div className="badge">3</div>
           </div>
-        ) : (
-          ""
-        )}
+        </div>  
       </div>
+      <AddList showNewList={showNewList} handleShowNewList={handleShowNewList} />
     </div>
   );
 };
