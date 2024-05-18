a32a02a5af72c0c319cc774a7e5b0fc481c9ce18
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-18 19:55:34 +0300

    moved AllLists to a separated component

---
+++ src/components/AllLists/AllLists.jsx
@@ -0,0 +1,17 @@
+import "./AllLists.css"
+
+const AllLists = () => {
+  return (
+    <div className="all-lists">
+        <div className="single-list">
+          <div><p>My team</p></div>
+          <div className="notifier">
+          <i className="fa-solid fa-user-group"></i>
+          <div className="badge">3</div>
+          </div>
+        </div>  
+      </div>
+  )
+}
+
+export default AllLists
--- src/views/Contacts/Contacts.jsx
+++ src/views/Contacts/Contacts.jsx
@@ -1,6 +1,7 @@
 import "./Contacts.css";
 import { useState } from "react";
 import AddList from "../../components/AddList/AddList";
+import AllLists from "../../components/AllLists/AllLists";
 
 const Contacts = () => {
   const [showNewList, setShowNewList] = useState(false);
@@ -15,15 +16,7 @@ const Contacts = () => {
         <h3>Your contact lists:</h3>
         <button onClick={handleShowNewList}>Add contact list</button>
       </div>
-      <div className="all-lists">
-        <div className="single-list">
-          <div><p>My team</p></div>
-          <div className="notifier">
-          <i className="fa-solid fa-user-group"></i>
-          <div className="badge">3</div>
-          </div>
-        </div>  
-      </div>
+      <AllLists />
       <AddList showNewList={showNewList} handleShowNewList={handleShowNewList} />
     </div>
   );
