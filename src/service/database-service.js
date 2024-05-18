8b37b437581c23d7c4551d4fc18e8bf36cac62f9
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-18 18:50:38 +0300

    Add service/database-service.js for communication with Firebase realtime database

---
+++ src/service/database-service.js
@@ -0,0 +1,30 @@
+import database from '../config/firebase-config.js';
+import { ref, get, set, update, query, equalTo, orderByChild, goOnline } from "firebase/database";
+
+/**
+ * Checks if a user exists in the database.
+ * @param {string} username - The username of the user to check.
+ * @returns {Promise<Snapshot | string>} - A promise that resolves to a snapshot of the user data if the user exists, or an error message if the user does not exist.
+ */
+export const checkIfUserExists = async (username) => {
+  goOnline(database);
+  try {
+    const snapshot = await get(ref(database, `users/${username}`));
+    return snapshot;
+  } catch (error) {
+      console.log(error.message);
+    } 
+}
+
+/**
+ * Creates a new user in the database.
+ * @param {Object} userDetails - The details of the user to be created.
+ * @returns {Promise<string|undefined>} - A promise that resolves to the created user's details or an error message if an error occurs.
+ */
+export const createUser = async (userDetails) => {
+  try {
+    return await set(ref(database, `users/${userDetails.username}`), userDetails);
+  } catch (error) {
+      console.log(error.message);
+  }
+}
