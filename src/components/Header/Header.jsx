6162eb7b53d85491cfa23e107d01100b860f1581
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-18 20:22:26 +0300

    Add event handlers for Log in/Get started anchors in Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import "./Header.css"
 import { assets } from "../../assets/assets"
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLoginClick = (event) => {
+    event.preventDefault();
+    navigate(`/login`);
+  }
+
+  const handleRegisterClick = (event) => {
+    event.preventDefault();
+    navigate(`/register`);
+  }
+
   return (
     <>
     <div className="header">
@@ -9,8 +22,8 @@ const Header = () => {
             <img src={assets.logo} alt="logo" />
         </div>
         <div className="header-menu">
-            <a id="loginBtn" href="/login">Log in</a>
-            <a id="registerBtn" href="/signup">Get started</a>
+            <a id="loginBtn" href="#" onClick={handleLoginClick}>Log in</a>
+            <a id="registerBtn" href="#" onClick={handleRegisterClick}>Get started</a>
         </div>
     </div>
     <hr />
