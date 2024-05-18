f8b9a6324d2b2f0efeaf3b90fab1fd85b1b5d404
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-18 19:12:55 +0300

    Import useState() hook to App.jsx and add state with it to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import './App.css'
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PublicView from './views/PublicView/PublicView';
+import Header from './components/Header/Header';
+import Login from './components/Login/Login';
+import Logout from './components/Logout/Logout';
+import Register from './components/Register/Register';
+import AuthContext from './context/AuthContext';
+import './App.css'
 
 const App = () => {
 
+  const[authValue, setAuthValue] = useState({
+    status: false,
+    user: ''
+  });
+
   return (
     <BrowserRouter>
+      <AuthContext.Provider value={{isLoggedIn: authValue, setLoginState: setAuthValue}}>
       <Routes>
-        <Route path="/" element={<PublicView />} />
+          <Route path="/" element={<PublicView />} />
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+          <Route path="/logout" element={<Logout />}/>
+         {/*  <Route path="/posts" element={<Authenticated><Posts /></Authenticated>}/>
+          <Route path="/posts/:id" element={<Authenticated><SinglePost /></Authenticated>}/>
+          <Route path="/create-post" element={<Authenticated><CreatePost /></Authenticated>}/>
+          <Route path="/users" element={<Authenticated><Users /></Authenticated>}/>
+          <Route path="/profile" element={<Authenticated><Profile /></Authenticated>}/>
+          <Route path="*" element={<NotFound />}/> */}
       </Routes>
+      </AuthContext.Provider>
     </BrowserRouter>
   )
 }
