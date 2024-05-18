50bbda49289a49672598ab15529b9d765401f945
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-18 19:41:08 +0300

    made the addList form different component

---
+++ src/components/AddList/AddList.jsx
@@ -0,0 +1,51 @@
+import "./AddList.css"
+import PropTypes from 'prop-types';
+import "./AddList.css"
+
+const AddList = ({showNewList, handleShowNewList}) => {
+  return (
+    <div><div className="container-list-form">
+    {showNewList ? (
+      <div className="add-list">
+        <div className="add-list__header">
+          <span className="add-list__title">New list</span>
+          <button onClick={handleShowNewList} className="button--icon">x</button>
+        </div>
+        <div className="add-list__body">
+          <div className="input">
+            <label className="input__label">List name</label>
+            <input className="input__field" type="text" placeholder="Enter list name" />
+          </div>
+          <div className="input">
+            <label className="input__label">Members</label>
+            <input className="input__field" type="search" placeholder="Add members by email" />
+          </div>
+        </div>
+        <div className="container-find-contact">
+          <div className="single-contact">
+            <div>
+              <img src="https://picsum.photos/40/40"></img>
+            </div>
+            <div>
+              <p>petko1234@gmail.com</p>
+            </div>
+            <button>Add</button>
+          </div>
+        </div>
+        <div className="add-list__footer">
+          <button className="button--primary">Create list</button>
+        </div>
+      </div>
+    ) : (
+      ""
+    )}
+  </div></div>
+  )
+}
+
+AddList.propTypes = {
+    showNewList: PropTypes.bool.isRequired,
+    handleShowNewList: PropTypes.func.isRequired
+};
+
+export default AddList
