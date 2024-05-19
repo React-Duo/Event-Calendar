1a55e98d2eefb5f2af2da6ab0e46b0aeebac27e7
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-19 23:24:16 +0300

    AllLists now receive the real lists from the database

--- src/components/AllLists/AllLists.jsx
+++ src/components/AllLists/AllLists.jsx
@@ -1,17 +1,47 @@
 import "./AllLists.css"
+import { getLists } from "../../service/database-service"
+import { useEffect, useState, useContext } from "react"
+import AuthContext from "../../context/AuthContext";
+import PropTypes from 'prop-types';
 
-const AllLists = () => {
+const AllLists = ({triggerRefetch}) => {
+  const [lists, setLists] = useState([]);
+  const { isLoggedIn } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchLists = async () => {
+      try {
+        let lists = await getLists();
+        lists = Object.values(lists);
+        const myLists = lists.filter((list) => list.owner === isLoggedIn.user);
+        setLists(myLists);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    console.log(lists);
+    fetchLists();
+  }, [isLoggedIn.user, triggerRefetch]);
+
   return (
     <div className="all-lists">
-        <div className="single-list">
-          <div><p>My team</p></div>
-          <div className="notifier">
-          <i className="fa-solid fa-user-group"></i>
-          <div className="badge">3</div>
+      {lists.map((list, index) => (
+        <div key={index} className="single-list">
+          <div>
+            <p>{list.name}</p>
           </div>
-        </div>  
-      </div>
+          <div className="notifier">
+            <i className="fa-solid fa-user-group"></i>
+            <div className="badge">3</div>
+          </div>
+        </div>
+      ))}
+    </div>
   )
+}
+
+AllLists.propTypes = {
+  triggerRefetch: PropTypes.bool.isRequired
 }
 
 export default AllLists
