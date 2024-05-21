17cca240f8fb5f33275099e882e84f378e2e1d60
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-21 21:20:46 +0300

    Add navigate() to /add-event for Add Event list item in SideBar.jsx

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -13,7 +13,7 @@ const SideBar = () => {
       <img src={assets.logo}></img>
         <li onClick={() => navigate("/home")} ><i className="fa-solid fa-house fa-xl"></i>Home</li>
         <li><i className="fa-solid fa-calendar-days fa-xl"></i>Calendar</li>
-        <li><i className="fa-solid fa-square-check fa-xl"></i>Add Event</li>
+        <li onClick={() => navigate("/add-event")}><i className="fa-solid fa-square-check fa-xl"></i>Add Event</li>
         <li onClick={() => navigate("/contacts")}><i className="fa-solid fa-address-book fa-xl"></i>Contacts</li>
         <li><i className="fa-solid fa-gear fa-xl"></i>Settings</li>
       </ul>
