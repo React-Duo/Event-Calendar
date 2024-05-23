46a9c6632dfc2732e01f042af1d8f0c5776a9306
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-23 09:28:26 +0300

    added navigate to the calendar view

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -12,7 +12,7 @@ const SideBar = () => {
       <ul className="side-bar-tags">
       <img src={assets.logo}></img>
         <li onClick={() => navigate("/home")} ><i className="fa-solid fa-house fa-xl"></i>Home</li>
-        <li><i className="fa-solid fa-calendar-days fa-xl"></i>Calendar</li>
+        <li onClick={() => navigate("/calendar")}><i className="fa-solid fa-calendar-days fa-xl"></i>Calendar</li>
         <li onClick={() => navigate("/add-event")}><i className="fa-solid fa-square-check fa-xl"></i>Add Event</li>
         <li onClick={() => navigate("/contacts")}><i className="fa-solid fa-address-book fa-xl"></i>Contacts</li>
         <li><i className="fa-solid fa-gear fa-xl"></i>Settings</li>
