802aa3dccbafc008e5ff4c7e02d342dfeb3854a0
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-23 22:52:21 +0300

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer className="container-footer">
+        <p>Copyright Calendo 2024</p>
+    </footer>
+  )
+}
+
+export default Footer
