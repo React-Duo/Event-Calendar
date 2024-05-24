b7b59d1be80a4008aaf61ab86f30b9336568e2b1
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-24 20:01:52 +0300

    Remove handleWeekdayChange() fn definition from Weekdays.jsx as it is passed via props from AddEvent.jsx (parent)

--- src/components/AddEvent/Weekdays.jsx
+++ src/components/AddEvent/Weekdays.jsx
@@ -1,27 +1,23 @@
 import './Weekdays.css';
 
-const Weekdays = () => {
-
-    const handleWeekdayChange = (e) => {
-        console.log(e.target.value);
-    }
+const Weekdays = (props) => {
 
     return (
         <>
             <div className="weekDays-selector">
-                <input type="checkbox" id="weekday-mon" value="Monday" className="weekday" onChange={handleWeekdayChange}/>
+                <input type="checkbox" id="weekday-mon" value="Monday" className="weekday" onChange={props.handle}/>
                 <label htmlFor="weekday-mon">M</label>
-                <input type="checkbox" id="weekday-tue" value="Tuesday" className="weekday" onChange={handleWeekdayChange}/>
+                <input type="checkbox" id="weekday-tue" value="Tuesday" className="weekday" onChange={props.handle}/>
                 <label htmlFor="weekday-tue">T</label>
-                <input type="checkbox" id="weekday-wed" value="Wednesday" className="weekday" onChange={handleWeekdayChange}/>
+                <input type="checkbox" id="weekday-wed" value="Wednesday" className="weekday" onChange={props.handle}/>
                 <label htmlFor="weekday-wed">W</label>
-                <input type="checkbox" id="weekday-thu" value="Thursday" className="weekday" onChange={handleWeekdayChange}/>
+                <input type="checkbox" id="weekday-thu" value="Thursday" className="weekday" onChange={props.handle}/>
                 <label htmlFor="weekday-thu">T</label>
-                <input type="checkbox" id="weekday-fri" value="Friday" className="weekday" onChange={handleWeekdayChange}/>
+                <input type="checkbox" id="weekday-fri" value="Friday" className="weekday" onChange={props.handle}/>
                 <label htmlFor="weekday-fri">F</label>
-                <input type="checkbox" id="weekday-sat" value="Saturday" className="weekday" onChange={handleWeekdayChange}/>
+                <input type="checkbox" id="weekday-sat" value="Saturday" className="weekday" onChange={props.handle}/>
                 <label htmlFor="weekday-sat">S</label>
-                <input type="checkbox" id="weekday-sun" value="Sunday" className="weekday" onChange={handleWeekdayChange}/>
+                <input type="checkbox" id="weekday-sun" value="Sunday" className="weekday" onChange={props.handle}/>
                 <label htmlFor="weekday-sun">S</label>
             </div>
             <label htmlFor="weekDays-selector"> Select day </label>
