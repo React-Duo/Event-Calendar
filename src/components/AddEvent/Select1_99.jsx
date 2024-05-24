2505d8b23976563122104d6e4f9b9fb71d2c14aa
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-24 19:55:58 +0300

    Remove handleFreqChange() fn definition from Select1_99.jsx as it is passed now via props from AddEvent.jsx (parent)

--- src/components/AddEvent/Select1_99.jsx
+++ src/components/AddEvent/Select1_99.jsx
@@ -3,13 +3,9 @@ const Select1_99 = (props) => {
     for (let i = 1; i < 100; i++) {
         options.push(<option key={i} value={i}>{i}</option>);
     }
-
-    const handleFreqChange = (e) => {
-        console.log(e.target.value);
-    }
     
     return (
-        <select name={props.freq} id={props.freq} onChange={handleFreqChange} className="common">
+        <select name={props.freq} id={props.freq} onChange={props.handle} className="common">
         {options}
         </select>
     );
