c9d1b5c68cd8205d057cc2f28075bbd3efd37807
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-24 19:34:38 +0300

    Add Select1_99.jsx component in src/components/AddEvent to generate 99 options for day/week/month

--- src/components/AddEvent/Select1_99.jsx
+++ src/components/AddEvent/Select1_99.jsx
@@ -3,9 +3,13 @@ const Select1_99 = (props) => {
     for (let i = 1; i < 100; i++) {
         options.push(<option key={i} value={i}>{i}</option>);
     }
+
+    const handleFreqChange = (e) => {
+        console.log(e.target.value);
+    }
     
     return (
-        <select name={props.freq} id={props.freq} className="common">
+        <select name={props.freq} id={props.freq} onChange={handleFreqChange} className="common">
         {options}
         </select>
     );
