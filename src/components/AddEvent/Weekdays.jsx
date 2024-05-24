55ebb0e3d97badb806e55fd609769fb8590b2cc7
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-24 19:35:25 +0300

    Add Weekday.jsx component in src/components/AddEvent to show days of the week when event repeats every week

--- src/components/AddEvent/Weekdays.jsx
+++ src/components/AddEvent/Weekdays.jsx
@@ -2,23 +2,30 @@ import './Weekdays.css';
 
 const Weekdays = () => {
 
+    const handleWeekdayChange = (e) => {
+        console.log(e.target.value);
+    }
+
     return (
-        <div class="weekDays-selector">
-            <input type="checkbox" id="weekday-mon" className="weekday" />
-            <label htmlFor="weekday-mon">M</label>
-            <input type="checkbox" id="weekday-tue" className="weekday" />
-            <label htmlFor="weekday-tue">T</label>
-            <input type="checkbox" id="weekday-wed" className="weekday" />
-            <label htmlFor="weekday-wed">W</label>
-            <input type="checkbox" id="weekday-thu" className="weekday" />
-            <label htmlFor="weekday-thu">T</label>
-            <input type="checkbox" id="weekday-fri" className="weekday" />
-            <label htmlFor="weekday-fri">F</label>
-            <input type="checkbox" id="weekday-sat" className="weekday" />
-            <label htmlFor="weekday-sat">S</label>
-            <input type="checkbox" id="weekday-sun" className="weekday" />
-            <label htmlFor="weekday-sun">S</label>
-        </div>
+        <>
+            <div className="weekDays-selector">
+                <input type="checkbox" id="weekday-mon" value="Monday" className="weekday" onChange={handleWeekdayChange}/>
+                <label htmlFor="weekday-mon">M</label>
+                <input type="checkbox" id="weekday-tue" value="Tuesday" className="weekday" onChange={handleWeekdayChange}/>
+                <label htmlFor="weekday-tue">T</label>
+                <input type="checkbox" id="weekday-wed" value="Wednesday" className="weekday" onChange={handleWeekdayChange}/>
+                <label htmlFor="weekday-wed">W</label>
+                <input type="checkbox" id="weekday-thu" value="Thursday" className="weekday" onChange={handleWeekdayChange}/>
+                <label htmlFor="weekday-thu">T</label>
+                <input type="checkbox" id="weekday-fri" value="Friday" className="weekday" onChange={handleWeekdayChange}/>
+                <label htmlFor="weekday-fri">F</label>
+                <input type="checkbox" id="weekday-sat" value="Saturday" className="weekday" onChange={handleWeekdayChange}/>
+                <label htmlFor="weekday-sat">S</label>
+                <input type="checkbox" id="weekday-sun" value="Sunday" className="weekday" onChange={handleWeekdayChange}/>
+                <label htmlFor="weekday-sun">S</label>
+            </div>
+            <label htmlFor="weekDays-selector"> Select day </label>
+        </>
     )
 }
 
