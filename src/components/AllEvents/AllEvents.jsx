0d71ff03b756e37b07683d551924f19312aae77b
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-25 09:59:48 +0300

    users can join events

--- src/components/AllEvents/AllEvents.jsx
+++ src/components/AllEvents/AllEvents.jsx
@@ -1,5 +1,5 @@
 import "./AllEvents.css"
-import { getAllEvents } from "../../service/database-service"
+import { getAllEvents, addUserToEvent } from "../../service/database-service"
 import { useEffect, useState, useContext } from "react"
 import AuthContext from "../../context/AuthContext";
 
@@ -10,13 +10,14 @@ const AllEvents = () => {
     const [filter, setFilter] = useState("");
     const [eventsToShow, setEventsToShow] = useState([]);
     const { isLoggedIn } = useContext(AuthContext);
+    const [showedEvents, setShowedEvents] = useState("All events");
 
 
     useEffect(() => {
         const fetchEvents = async () => {
 
             const events = await getAllEvents();
-            const publicEvents = events.filter(event => event.visibility === "public");
+            const publicEvents = events.filter(event => event[1].visibility === "public");
             setEvents(publicEvents);
             setEventsToShow(publicEvents);
         };
@@ -25,20 +26,32 @@ const AllEvents = () => {
        
     }, []);
 
+    const fetchAddUserToEvent = async (eventId) => {
+        await addUserToEvent(eventId, isLoggedIn.user);
+        const events = await getAllEvents();
+        const publicEvents = events.filter(event => event[1].visibility === "public");
+        setEvents(publicEvents);
+        setEventsToShow(publicEvents);
+        setFilter("joined")
+    };
+
    useEffect(() => {
     const filterEvents = (filter) => {
         if (filter === "top") {
-            const topEvents = events.sort((a, b) => b.invited.length - a.invited.length);
+            const topEvents = events.sort((a, b) => b[1].invited.length - a[1].invited.length);
             setEventsToShow([...topEvents]);
+            setShowedEvents("Top events")
         }
         if (filter === "today") {
-            const todayEvents = events.filter(event => event.startDate === new Date().toISOString().split('T')[0]);
+            const todayEvents = events.filter(event => event[1].startDate === new Date().toISOString().split('T')[0]);
             setEventsToShow([...todayEvents]);
+            setShowedEvents("Today events")
         }
 
         if (filter === "joined") {
-            const joinedEvents = events.filter(event => event.invited.includes(isLoggedIn.user));
+            const joinedEvents = events.filter(event => event[1].invited.includes(isLoggedIn.user));
             setEventsToShow([...joinedEvents]);
+            setShowedEvents("Joined events")
         }
     }
     filterEvents(filter);
@@ -48,6 +61,7 @@ const AllEvents = () => {
         <div className="all-events-container">
             <div>
                 <h2>Discover events</h2>
+                <h3>{showedEvents}</h3>
             </div>
             <div className="events-filter">
                 <p><i className="fa-solid fa-location-dot"></i>My location</p>
@@ -63,27 +77,27 @@ const AllEvents = () => {
                                 <img src="https://picsum.photos/380/200" alt="event" />
                             </div>
                             <div className="single-event-details">
-                                <h3>{event.title}</h3>
+                                <h3>{event[1].title}</h3>
                                 <div className="hours-event">
-                                    <p>From: <span >{`${event.startDate}th ${event.startTime}`}</span></p>
-                                    <p>To:<span>{`  ${event.endDate}th ${event.endTime}`}</span></p>
+                                    <p>From: <span >{`${event[1].startDate}th ${event[1].startTime}`}</span></p>
+                                    <p>To:<span>{`  ${event[1].endDate}th ${event[1].endTime}`}</span></p>
                                 </div>
-                                <p>{event.location}</p>
+                                <p>{event[1].location}</p>
                                 <div className="people-going">
-                                    {event.invited && event.invited.slice(0, 2).map((person, index) => (
+                                    {event[1].invited && event[1].invited.filter(person => person !== isLoggedIn.user).slice(0, 2).map((person, index) => (
                                         <div className="one-person" key={index}>
                                             <p>{`${person} is going  `}</p>
                                         </div>
                                     ))}
-                                    {event.invited && event.invited.length > 2 && (
+                                    {event[1].invited && event[1].invited.length > 2 && (
                                         <div className="one-person">
-                                            <p>{`  and ${event.invited.length - 2} more..`}</p>
+                                            <p>{`  and ${event[1].invited.length - 2} more..`}</p>
                                         </div>
                                     )}
                                 </div>
                                 <div className="single-event-options">
-                                    <button className="btn">More info</button>
-                                    <button className="btn">Join</button>
+                                    <button className="btn" disabled>More info</button>
+                                    {event[1].invited.includes(isLoggedIn.user) ? <button className="btn" style={{ color: "green" }} disabled>Joined</button> : <button className="btn"  onClick={() => fetchAddUserToEvent(event[0])}>Join</button>}
                                 </div>
                             </div>
                         </div>
