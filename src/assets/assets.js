f75efb52fed267150e9fe337b941139a51f028fd
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-31 18:09:41 +0300

    added company logos for the public view

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,8 +1,32 @@
 import logo from "./Logo.png";
 import favicon from "./favicon.png";
+import publicHeader from "./discoverEvents.png";
+import company1 from "./company1.png";
+import company2 from "./company2.png";
+import company3 from "./company3.png";
+import company4 from "./company4.png";
+import company5 from "./company5.png";
+import company6 from "./company6.png";
+import company7 from "./company7.png";
+import company8 from "./company8.png";
+import company9 from "./company9.png";
+import company10 from "./company10.png";
+
+
 
 export const assets = {
     logo,
     favicon,
+    publicHeader,
+    company1,
+    company2,
+    company3,
+    company4,
+    company5,
+    company6,
+    company7,
+    company8,
+    company9,
+    company10,
 };
 
