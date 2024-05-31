a9e289844ff470c9cc8f31a6ce76d4e15ec6bd02
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-31 18:36:21 +0300

    added icons to the footer form more style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,23 @@ import "./Footer.css"
 const Footer = () => {
   return (
     <footer className="container-footer">
-        <p>Copyright Calendo 2024</p>
+      <div> <p>Copyright Calendo 2024</p></div>
+      <div>
+        <ul className="wrapper">
+          <a href="https://www.facebook.com/" target="_blank" className="icon facebook">
+            <span className="tooltip">Facebook</span>
+            <i className="fa-brands fa-facebook-f fa-lg"></i>
+          </a>
+          <a href="https://x.com/" target="_blank" className="icon twitter">
+            <span className="tooltip">Twitter(X)</span>
+            <i className="fa-brands fa-x-twitter fa-lg"></i>
+          </a>
+          <a href="https://www.instagram.com/" target="_blank" className="icon instagram">
+            <span className="tooltip">Instagram</span>
+            <i className="fa-brands fa-instagram fa-lg"></i>
+          </a>
+        </ul>
+      </div>
     </footer>
   )
 }
