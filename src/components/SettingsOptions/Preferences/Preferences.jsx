e42ba1b5f76b99107d823d4a182479160d105ae2
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-03 12:20:37 +0300

    feat: Add invite preference functionality to Preferences component (if user checks the input this adds an invitePreference property with value "true" to the user's object

--- src/components/SettingsOptions/Preferences/Preferences.jsx
+++ src/components/SettingsOptions/Preferences/Preferences.jsx
@@ -1,15 +1,50 @@
 import "./Preferences.css"
+import { useState, useContext, useEffect } from "react"
+import { editCredential, getUserDetails } from "../../../service/database-service";
+import AuthContext from "../../../context/AuthContext";
 
 const Preferences = () => {
+
+  const { isLoggedIn } = useContext(AuthContext);
+  const [invitePreference, setInvitePreference] = useState(false);
+  const [userDetails, setUserDetails] = useState({});
+  useEffect(() => {
+    const getUser = async () => {
+      const userDetails = await getUserDetails(isLoggedIn.user);
+      if (userDetails[0].invitePreference) {
+        setInvitePreference(true);
+      }
+      setUserDetails(userDetails[0]);
+    }
+    getUser();
+  }, [isLoggedIn.user]);
+
+
+  const handleCheckboxChange = async (event) => {
+    if (event.target.checked) {
+      await editCredential(userDetails.username, "invitePreference", "true")
+      setInvitePreference(true);
+    } else {
+      await editCredential(userDetails.username, "invitePreference", null)
+      setInvitePreference(false);
+    }
+  };
+
   return (
     <div>
       <h2>Preferences</h2>
       <div className="preference">
         <label htmlFor="invite-preference">Dont invite me to an event unless I allow it:</label>
-        <input type="checkbox" id="invite-preference" name="invite-preference" />
+        <input 
+          type="checkbox" 
+          id="invite-preference" 
+          name="invite-preference" 
+          checked={invitePreference} 
+          onChange={handleCheckboxChange} 
+        />
       </div>
     </div>
   )
 }
 
-export default Preferences
+export default Preferences;
