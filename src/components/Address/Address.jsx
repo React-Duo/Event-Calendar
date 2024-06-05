f314097dfe18705093531fcac3d1f7d9da0dab29
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-06-06 00:24:23 +0300

    Add props to Address component so it can be called from SingleEvent.jsx and make input address fields required

--- src/components/Address/Address.jsx
+++ src/components/Address/Address.jsx
@@ -1,19 +1,18 @@
 import { STREET_MIN_CHARS, STREET_MAX_CHARS, COUNTRY_MIN_CHARS, COUNTRY_MAX_CHARS, CITY_MIN_CHARS, CITY_MAX_CHARS} from '../../common/constants';
 import './Address.css';
 
-const Address = () => {
+const Address = (props) => {
 
     return (
         <>
-            <label htmlFor="address"> Location </label>
             <span className="address">
-                <input type="text" name="country" id="country" placeholder="Country"/>
+                <input type="text" name="country" id="country" placeholder="Country" {...props.from === "singleEvent" && {required: true}} />
                 <h5><i>{COUNTRY_MIN_CHARS}-{COUNTRY_MAX_CHARS} chars | upper-/lowercase | space</i></h5>
 
-                <input type="text" name="city" id="city" placeholder="City"/>
+                <input type="text" name="city" id="city" placeholder="City" {...props.from === "singleEvent" && {required: true}} />
                 <h5><i>{CITY_MIN_CHARS}-{CITY_MAX_CHARS} chars | upper-/lowercase | space</i></h5>
 
-                <input type="text" name="street" id="street" placeholder="Street"/>
+                <input type="text" name="street" id="street" placeholder="Street" {...props.from === "singleEvent" && {required: true}} />
                 <h5><i>{STREET_MIN_CHARS}-{STREET_MAX_CHARS} chars | upper-/lowercase | digits | space | dot</i></h5>
             </span>
         </>
