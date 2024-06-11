d2a3a902eaa07fb28067e362d119ba0ed5b87ab1
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-06-11 22:55:20 +0300

    Update Address.jsx to include "defaultValue" for country, city and street from its props

--- src/components/Address/Address.jsx
+++ src/components/Address/Address.jsx
@@ -1,18 +1,21 @@
 import { STREET_MIN_CHARS, STREET_MAX_CHARS, COUNTRY_MIN_CHARS, COUNTRY_MAX_CHARS, CITY_MIN_CHARS, CITY_MAX_CHARS} from '../../common/constants';
 import './Address.css';
 
-const Address = () => {
+const Address = (props) => {
 
     return (
         <>
             <span className="address">
-                <input type="text" name="country" id="country" placeholder="Country" required />
+                <input type="text" name="country" id="country" placeholder="Country" required 
+                    defaultValue={props.from === "singleEvent" ? props.country : ''} />
                 <span><i>{COUNTRY_MIN_CHARS}-{COUNTRY_MAX_CHARS} chars | upper-/lowercase | space</i></span>
 
-                <input type="text" name="city" id="city" placeholder="City" required />
+                <input type="text" name="city" id="city" placeholder="City" required  
+                    defaultValue={props.from === "singleEvent" ? props.city : ''} />
                 <span><i>{CITY_MIN_CHARS}-{CITY_MAX_CHARS} chars | upper-/lowercase | space</i></span>
 
-                <input type="text" name="street" id="street" placeholder="Street" required/>
+                <input type="text" name="street" id="street" placeholder="Street" required
+                    defaultValue={props.from === "singleEvent" ? props.street : ''} />
                 <span><i>{STREET_MIN_CHARS}-{STREET_MAX_CHARS} chars | upper-/lowercase | digits | space | dot</i></span>
             </span>
         </>
