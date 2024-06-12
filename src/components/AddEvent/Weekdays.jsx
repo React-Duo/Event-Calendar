22a1522dd8c64b282d4b4bc83bb151653a83df8f
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-06-12 19:47:15 +0300

    Remove props from Weekdays.jsx as they are not in use anyway

--- src/components/AddEvent/Weekdays.jsx
+++ src/components/AddEvent/Weekdays.jsx
@@ -1,25 +1,25 @@
 import './Weekdays.css';
 import PropTypes from 'prop-types';
 
-const Weekdays = (props) => {
+const Weekdays = () => {
 
     return (
         <>
             <label htmlFor="weekDays-selector"> Days: </label>
             <span className="weekDays-selector">
-                <input type="checkbox" id="monday" onChange={props.handle}/>
+                <input type="checkbox" id="monday" />
                 <label htmlFor="monday">Mon</label>
-                <input type="checkbox" id="tuesday" onChange={props.handle}/>
+                <input type="checkbox" id="tuesday" />
                 <label htmlFor="tuesday">Tue</label>
-                <input type="checkbox" id="wednesday" onChange={props.handle}/>
+                <input type="checkbox" id="wednesday" />
                 <label htmlFor="wednesday">Wed</label>
-                <input type="checkbox" id="thursday" onChange={props.handle}/>
+                <input type="checkbox" id="thursday" />
                 <label htmlFor="thursday">Thu</label>
-                <input type="checkbox" id="friday" onChange={props.handle}/>
+                <input type="checkbox" id="friday" />
                 <label htmlFor="friday">Fri</label>
-                <input type="checkbox" id="saturday" onChange={props.handle}/>
+                <input type="checkbox" id="saturday" />
                 <label htmlFor="saturday">Sat</label>
-                <input type="checkbox" id="sunday" onChange={props.handle}/>
+                <input type="checkbox" id="sunday" />
                 <label htmlFor="sunday">Sun</label>
             </span>
         </>
