0680a8669d463853f9c9f97c67207addf9493ad4
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-12 09:42:07 +0300

    added onClick function on the logo that navigates to the home view

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -6,15 +6,17 @@ import AuthContext from "../../context/AuthContext";
 
 const SideBar = () => {
   const navigate = useNavigate();
- const {theme} = useContext(AuthContext);
+  const { theme } = useContext(AuthContext);
 
   const currentPath = window.location.pathname;
 
   return (
-    <div className={`side-bar-container ${theme && "dark-theme-sideBar" }`}>
+    <div className={`side-bar-container ${theme && "dark-theme-sideBar"}`}>
       <ul className="side-bar-tags">
-        <img src={assets.logo}></img>
-        <li className={currentPath === "/home" ? "activePath" : ""} onClick={() => navigate("/home")}><i className="fa-solid fa-house fa-xl"></i>Home</li>
+        <img onClick={() => {
+          navigate("/home");
+          window.scrollTo(0, 0);
+        }} src={assets.logo}></img>        <li className={currentPath === "/home" ? "activePath" : ""} onClick={() => navigate("/home")}><i className="fa-solid fa-house fa-xl"></i>Home</li>
         <li className={currentPath === "/calendar" ? "activePath" : ""} onClick={() => navigate("/calendar")}><i className="fa-solid fa-calendar-days fa-xl"></i>Calendar</li>
         <li className={currentPath === "/add-event" ? "activePath" : ""} onClick={() => navigate("/add-event")}><i className="fa-solid fa-square-check fa-xl"></i>Add Event</li>
         <li className={currentPath === "/contacts" ? "activePath" : ""} onClick={() => navigate("/contacts")}><i className="fa-solid fa-address-book fa-xl"></i>Contacts</li>
